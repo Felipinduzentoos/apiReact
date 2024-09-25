cb570888045ff469d1c211d8e5cfa7e38ac18a01
Felipinduzentoos <felipin200dedano@gmail.com>
2024-09-25 12:12:47 -0300

    commit1

--- pizzaria2a/src/App.jsx
+++ pizzaria2a/src/App.jsx
@@ -4,24 +4,24 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import loadingGif from './Rolling@1x-1.0s-200px-200px.gif';
 function App() {
-  const [images, setImages] = useState(''); // Inicializa o estado como string vazia
+  const [images, setImages] = useState(''); 
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
       .then(data => {
-        setImages(data.message); // Acesse a imagem na propriedade 'message'
+        setImages(data.message); 
         setLoading(false)
       })
       .catch(error => {
         console.error('Error fetching dog image:', error);
         setLoading(false)
       });
-  }, []); // O array vazio garante que o efeito só rode uma vez
+  }, []); 
 
   return (
     <div>
-    <h1>Random Dog Image</h1>
+    <h1>Olá josé</h1>
     {loading ? (
       <img 
         src={loadingGif}
